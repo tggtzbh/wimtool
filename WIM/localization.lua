fe0324f9f1d724c3ded0e0deb9bea781e36978e1
tggtzbh <tggtzbh@sina.com>
2024-04-23 18:00:27 +0800

    basic version

---
+++ WIM/localization.lua
@@ -0,0 +1,72 @@
+
+	BINDING_HEADER_WIMMOD = "WIM (WoW Instant Messenger)";
+	BINDING_NAME_WIMSHOWNEW = "Show New Messages";
+	BINDING_NAME_WIMHISTORY = "History Viewer";
+	BINDING_NAME_WIMENABLE = "Enable/Disable";
+	BINDING_NAME_WIMTOGGLE = "Toggle Messages";
+	BINDING_NAME_WIMSHOWALL = "Show All Messages";
+	BINDING_NAME_WIMHIDEALL = "Hide All Messages";
+
+	-- Class Names
+	WIM_LOCALIZED_DRUID = "Druid";
+	WIM_LOCALIZED_HUNTER = "Hunter";
+	WIM_LOCALIZED_MAGE = "Mage";
+	WIM_LOCALIZED_PALADIN = "Paladin";
+	WIM_LOCALIZED_PRIEST = "Priest";
+	WIM_LOCALIZED_ROGUE = "Rogue";
+	WIM_LOCALIZED_SHAMAN = "Shaman";
+	WIM_LOCALIZED_WARLOCK = "Warlock";
+	WIM_LOCALIZED_WARRIOR = "Warrior";
+
+WIM_DESCRIPTION = [[
+WIM (WoW Instant Messenger)|cffffffff未设置汉化（4903747）|r
+
+
+用户命令：
+/wim			|cffffffff- 设置窗口|r
+/wim history	|cffffffff- 查看历史消息|r
+/wim help		|cffffffff- 帮助窗口|r
+
+
+高级命令：
+/wim reset			|cffffffff- 恢复默认设置|r
+/wim reset filters	|cffffffff- 重置所有过滤器|r
+/wim clear history	|cffffffff- 清空历史记录|r
+
+
+WIM可以嵌入到泰坦面板之中
+]]
+
+WIM_DIDYOUKNOW = [[
+Did you know...|cffffffff By typing the slash command |r/wim |cffffffffyou open the options interface where you can customize the look and feel of WIM?|r
+
+Did you know...|cffffffff WIM comes with a built in Titan Panel plugin? Look for it in Titan's plugin menu!|r
+
+Did you know...|cffffffff If you go to "Key Bindings" on the "Main Menu", you can find a few useful key bindings for WIM?|r
+
+Did you know...|cffffffff You can make the minimap icon free moving? When in free moving mode, you can Shift-LeftClick the minimap icon to drag it anywhere you want.|r
+
+Did you know...|cffffffff By clicking the tab key while in a message, you can toggle between other messages as well?|r
+
+Did you know...|cffffffff I receive many coments from users everyday requesting features that are already implemented in WIM? It is very helpful to read the change log between updates. noobs! :-)|r
+
+Did you know...|cffffffff I am grateful to everyone who has taken the time to vote and send in their comments and suggestions? Well... I am :-).|r
+
+Did you know...|cffffffff I am a horrible speller? Please don't hesitate to send a correction my way!|r
+
+Did you know...|cffffffff WIM has so many options, that there is a scroll bar on the general options tab? Be sure to check it out. There may be some options available that you did not know about!|r
+]]
+
+WIM_CREDITS = [[
+WIM (WoW Instant Messenger) by Pazza <Bronzebeard>. 
+|cffffffffThe concept and ideas were originated by Sloans <Bronzebeard>.|r
+
+I would like to thank everyone who has helped test WIM as well as submit their feedback and suggestions including:
+|cffffffff
+- Nachonut <Bronzebeard>
+- Sloans <Bronzebeard>
+- Everyone in Resurgence <Bronzebeard>
+|r
+
+I would also like to thank everyone who has contributed on both ui.WorldOfWar.net and on Curse-Gaming.com.
+]]
